e1d03779c48e2766128f001daf12a352000d9fc1
Christopher MacDonald <sage_dev@outlook.com>
2017-11-09 07:54:44 +0400

    Creation of command container class

---
+++ Providence/commands.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace Providence
+{
+    public class commands
+    {
+
+    }
+}
