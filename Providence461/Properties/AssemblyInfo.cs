aa4b5155ea6d075498069e09bc971c13d68e78c2
Momo <sage_dev@outlook.com>
2017-11-13 13:06:58 +0400

    Added metadata (file version/author/title etc)

--- Providence461/Properties/AssemblyInfo.cs
+++ Providence461/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,10 +7,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Providence461")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("MomoLair Discord Bot")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Providence461")]
+[assembly: AssemblyCompany("ToyMakerSage")]
+[assembly: AssemblyProduct("Providence")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.2.0")]
+[assembly: AssemblyFileVersion("0.1.2.0")]
+[assembly: NeutralResourcesLanguage("en-GB")]
+
